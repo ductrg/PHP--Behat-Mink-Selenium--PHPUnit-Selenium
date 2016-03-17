f80f3fea51339a200971026091a4d90acfcd7717
trtanduc <duc.truong@dirox.net>
2016-03-17 12:05:17 +0700

    support travis

--- start-sele.sh
+++ start-sele.sh
@@ -1,4 +1,4 @@
 wget "http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar"
 nohup Xvfb :40 -ac &
 export DISPLAY=:40
-java -jar selenium-server-standalone-2.53.0.jar
+java -jar selenium-server-standalone-2.53.0.jar /tmp/selenium.log &
