8b58dca7d34928e21d0d514c49cd7d8d4ee120e5
trtanduc <duc.truong@dirox.net>
2016-03-17 15:27:46 +0700

    support travis

--- start-sele.sh
+++ start-sele.sh
@@ -1,7 +1,7 @@
 wget "http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar"
 nohup Xvfb :40 -ac &
 export DISPLAY=:40
-java -jar selenium-server-standalone-2.53.0.jar > /tmp/selenium.log &
+nohup java -jar selenium-server-standalone-2.53.0.jar > /tmp/selenium.log 2>&1 &
 wget --retry-connrefused --tries=5 --waitretry=3 "http://127.0.0.1:4444/wd/hub/status"
 if [ ! $? -eq 0 ]; then
     echo "Selenium server is not started properly"
