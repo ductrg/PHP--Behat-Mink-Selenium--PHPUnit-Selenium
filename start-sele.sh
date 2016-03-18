d0affe6258529c74147f8f069a112489be802cdf
trtanduc <duc.truong@dirox.net>
2016-03-18 18:06:08 +0700

    complete

--- start-sele.sh
+++ start-sele.sh
@@ -3,7 +3,7 @@ unzip chromedriver_linux64.zip
 wget "http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar"
 nohup Xvfb :40 -ac &
 export DISPLAY=:40
-java -jar selenium-server-standalone-2.53.0.jar -Dwebdriver.chrome.driver=chromedriver > /tmp/selenium.log &
+nohup java -jar selenium-server-standalone-2.53.0.jar -Dwebdriver.chrome.driver=chromedriver > /tmp/selenium.log 2>&1 &
 wget --retry-connrefused --tries=5 --waitretry=3 "http://127.0.0.1:4444/wd/hub/status"
 if [ ! $? -eq 0 ]; then
     echo "Selenium server is not started properly"
